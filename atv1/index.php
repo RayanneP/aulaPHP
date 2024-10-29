1d35bfa1ef14d686c5a03409641fe984c7b9f68e
RayanneP <rayannepicinidasilva@gmail.com>
2024-10-29 16:06:28 -0300

    Initial commit

---
+++ atv1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Mago</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+
+        echo "Hello, Mago🧙‍♂️";
+
+    ?>
+</body>
+</html>
